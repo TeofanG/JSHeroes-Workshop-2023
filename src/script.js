697ad365da6aee682f71eec98be490b481eb7b5d
teofang <teofang23@gmail.com>
2024-06-08 14:44:34 +0300

    up to date

--- src/script.js
+++ src/script.js
@@ -1,59 +1,83 @@
-const repositories = [
-  {
-    id: 28457823,
-    full_name: "freeCodeCamp/freeCodeCamp",
-    html_url: "https://github.com/freeCodeCamp/freeCodeCamp",
-    description:
-      "freeCodeCamp.org's open-source codebase and curriculum. Learn to code for free.",
-    forks: 30436,
-    stargazers_count: 357167,
-  },
-  {
-    id: 177736533,
-    full_name: "996icu/996.ICU",
-    html_url: "https://github.com/996icu/996.ICU",
-    description:
-      "Repo for counting stars and contributing. Press F to pay respect to glorious developers.",
-    stargazers_count: 264471,
-    forks: 21483,
-  },
-  {
-    id: 13491895,
-    full_name: "EbookFoundation/free-programming-books",
-    html_url: "https://github.com/EbookFoundation/free-programming-books",
-    description: ":books: Freely available programming books",
-    stargazers_count: 255304,
-    forks: 52494,
-  },
-  {
-    id: 60493101,
-    full_name: "jwasham/coding-interview-university",
-    html_url: "https://github.com/jwasham/coding-interview-university",
-    description:
-      "A complete computer science study plan to become a software engineer.",
-    stargazers_count: 239151,
-    forks: 63916,
-  },
-];
+const repoCardTemplate = document.querySelector("[data-repo-template]");
+const repoCardContainer = document.querySelector("[data-repo-cards-container]");
+const search_input = document.querySelector("[data-search");
+const button = document.getElementById("search-repos-btn");
+
+button.addEventListener("click", async (event) => {
+  event.preventDefault();
+  if (!search_input.value) {
+    alert('Empty search!');
+    return;
+  }
+  clearRepos();
+  await fetchRepositories((search_input).value);
+});
+
+search_input.addEventListener("input", async(event) =>{
+  const searchValue = event.target.value;
+  if(searchValue) {
+    clearRepos();
+    await fetchRepositories;
+  }
+});
 
 function createRepositoryCard() {
   // Implement string template HTML builder for repo card
 }
 
-function renderRepositories() {
+function renderRepositories(repository) {
+  //return repositories.map((repo) => {
+    // get the content inside the template / card
+    const card = repoCardTemplate.content.cloneNode(true).children[0];
+    const header = card.querySelector("[data-header]");
+    const description = card.querySelector("[data-description]");
+    const stars = card.querySelector("[data-stars]");
+    const forks = card.querySelector("[data-forks]");
+
+    description.textContent = repository.description;
+    header.textContent = repository.full_name;
+    stars.textContent = "Stars: " + repository.stargazers_count;
+    forks.textContent = "Forks: " + repository.forks;
+
+    repoCardContainer.append(card);
+  }
   // Implement DOM manipulation function to add list items in the repo list
-}
 
 // Comment this out when you start working on the search functionality
-renderRepositories();
+//renderRepositories();
 
 function handleSearch() {
   // Implement form submit event handler
 }
-
+/*
 async function fetchRepositories() {
   // Pass parameter to the search endpoint
   return fetch("https://api.github.com/legacy/repos/search/<placeholder>")
-    .then((res) => res.json())
-    .then((res) => res.repositories);
+    .then((res) => res.json());
+    //.then((res) => res.repositories;
+    .then(repositories =>{
+      repositories.items.map((repo)=>{
+        return renderRepositories(repo);
+      })
+  })
+}*/
+
+function fetchRepositories(searchParam = '') {
+  const API_URL = `https://api.github.com/search/repositories${searchParam ? '?q=' + searchParam : '?q=stars:>10000'}`;
+
+  return fetch(API_URL)
+    .then((response) => response.json())
+    .then(data => {
+      // hideLoadingMessage();
+
+      data.items.map((repo) => {
+        return renderRepositories(repo)
+      })
+    })
 }
+
+function clearRepos() {
+  repoCardContainer.textContent = null;
+}
+
+fetchRepositories();
